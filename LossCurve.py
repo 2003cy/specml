932591cdfb7b825161604ac530b4296d53a24212
Carmen Isabel Martinez Harris <ci.martharris@gmail.com>
2026-05-28 12:14:11 +0200

    Test 4
    n = 8000, lr = 1e-5, restarts = 4

--- LossCurve.py
+++ LossCurve.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the loss
-loss = np.load('loss_curve 20260528 n16000 lr1e-4 2restarts.npy')
+loss = np.load('loss_curve 20260528 n16000 lr5e-5 4restarts.npy')
 
 # Plot loss
 
-plt.title('Test 4: Loss Over 16000 Steps with a LR of 1e-4 over 2 Cosine Annealing Restarts')
+plt.title('Test 5: Loss Over 8000 Steps with a LR of 1e-5 over 4 Cosine Annealing Restarts')
 plt.xlabel('Number of Steps')
 plt.ylabel('Loss')
 plt.plot(loss, color = 'pink')
-plt.savefig('Test 4 20260528 n16000 lr1e-4 2restarts.')
+plt.savefig('Test 5 20260528 n16000 lr5e-5 4restarts')
 plt.show()
 
 
